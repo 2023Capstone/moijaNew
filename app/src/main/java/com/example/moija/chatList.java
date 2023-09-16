6ca0436db5bc26983169228afdaa76aafcf78b98
hojin535 <123153695+hojin535@users.noreply.github.com>
2023-09-17 00:15:54 +0900

    start

---
+++ app/src/main/java/com/example/moija/chatList.java
@@ -0,0 +1,14 @@
+package com.example.moija;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class chatList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_list);
+    }
+}
