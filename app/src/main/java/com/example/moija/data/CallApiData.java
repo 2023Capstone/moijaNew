8ada216f060c4a54cf2423950e046c1f8e873345
ENERGYMACARON <131278709+ENERGYMACARON@users.noreply.github.com>
2023-11-26 00:01:10 +0900

    Create CallApiData.java

---
+++ app/src/main/java/com/example/moija/data/CallApiData.java
@@ -0,0 +1,96 @@
+package com.example.moija.data;
+
+public class CallApiData {
+    double startPointX;
+    double startPointY;
+    double endPointX;
+    double endPointY;
+
+    //두번째 api 호출 데이터 저장
+    String startName;
+    String endName;
+    double startX;
+    double startY;
+    double endX;
+    double endY;
+
+    public double getStartPointX() {
+        return startPointX;
+    }
+
+    public void setStartPointX(double startPointX) {
+        this.startPointX = startPointX;
+    }
+
+    public double getStartPointY() {
+        return startPointY;
+    }
+
+    public void setStartPointY(double startPointY) {
+        this.startPointY = startPointY;
+    }
+
+    public double getEndPointX() {
+        return endPointX;
+    }
+
+    public void setEndPointX(double endPointX) {
+        this.endPointX = endPointX;
+    }
+
+    public double getEndPointY() {
+        return endPointY;
+    }
+
+    public void setEndPointY(double endPointY) {
+        this.endPointY = endPointY;
+    }
+
+    public String getStartName() {
+        return startName;
+    }
+
+    public void setStartName(String startName) {
+        this.startName = startName;
+    }
+
+    public String getEndName() {
+        return endName;
+    }
+
+    public void setEndName(String endName) {
+        this.endName = endName;
+    }
+
+    public double getStartX() {
+        return startX;
+    }
+
+    public void setStartX(double startX) {
+        this.startX = startX;
+    }
+
+    public double getStartY() {
+        return startY;
+    }
+
+    public void setStartY(double startY) {
+        this.startY = startY;
+    }
+
+    public double getEndX() {
+        return endX;
+    }
+
+    public void setEndX(double endX) {
+        this.endX = endX;
+    }
+
+    public double getEndY() {
+        return endY;
+    }
+
+    public void setEndY(double endY) {
+        this.endY = endY;
+    }
+}
